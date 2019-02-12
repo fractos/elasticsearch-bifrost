4e6e5553454ecb0684fa881ab5810f651f867a10
Adam Christie <fractos@gmail.com>
2019-02-12 23:14:04 +0000

    2.4.1 version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM elasticsearch:5.5.0
+FROM elasticsearch:2.4.1
 
 RUN apt-get update \
   && apt-get install -y python-pip \
@@ -10,5 +10,6 @@ RUN chmod +x /custom-entrypoint.sh
 ENTRYPOINT ["/custom-entrypoint.sh"]
 
 ENV AWS_REGION default
-ENV AWS_HOSTED_ZONE_ID default
+ENV AWS_ROUTE53_ZONE_ID default
 ENV AWS_ROUTE53_HOSTNAME default
+CMD ["elasticsearch"]
--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # get the container host's IP
+echo "Getting container host IP..."
+
 export LOCALIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4 2> /dev/null)
 
-# upsert a DNS record for the IP using the name we have been given in the specified hosted zone
+echo "Container host IP is $LOCALIP"
 
+# upsert a DNS record for the IP using the name we have been given in the specified hosted zone
 cat > /tmp/route53-record.txt <<EOFCAT
 {
   "Comment": "A new record set for the zone.",
@@ -26,7 +29,10 @@ cat > /tmp/route53-record.txt <<EOFCAT
 }
 EOFCAT
 
-aws route53 change-resource-record-sets --hosted-zone-id $AWS_HOSTED_ZONE_ID \
+echo "Updating Route53, mapping $AWS_ROUTE53_ZONE_ID/$AWS_ROUTE53_HOSTNAME to $LOCALIP..."
+
+aws route53 change-resource-record-sets --hosted-zone-id $AWS_ROUTE53_ZONE_ID \
   --change-batch file:///tmp/route53-record.txt --region $AWS_REGION
 
-elasticsearch -Etransport.host=0.0.0.0
+echo "Calling original entrypoint with $@"
+/docker-entrypoint.sh $@
